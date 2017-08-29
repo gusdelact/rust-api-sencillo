e17b3936aae15cc3b87937eac21241501fabbf58
Gustavo De la Cruz Tovar <gusdelact@Gustavo-De-la-Cruz-Tovars-MacBook.local>
2017-08-29 02:36:21 -0500

    se modifico la prueba unitaria con cURL

--- test.sh
+++ test.sh
@@ -1,29 +1,8 @@
 curl -X GET \
-  http://127.0.0.1:4000/api/v1/echo \
-  -d '{"greeting":"ayayay"}'
-#POST
-curl -X GET \
-  http://127.0.0.1:4000/api/v1/greet/gusdelact \
-  -H 'cache-control: no-cache' \
-  -H 'content-type: application/json' 
+  http://127.0.0.1:4000/api/v1/frikis 
 #POST
 curl -X POST \
-  http://127.0.0.1:4000/api/v1/greet/gusdelact \
+  http://127.0.0.1:4000/api/v1/friki/curlUser \
   -H 'cache-control: no-cache' \
   -H 'content-type: application/json' \
-  -d '{"greeting":"ayayay"}'
-#PUT
-curl -X PUT \
-  http://127.0.0.1:4000/api/v1/greet/gusdelact \
-  -H 'cache-control: no-cache' \
-  -H 'content-type: application/json' \
-  -d '{"greeting":"gusgus"}'
-#DELETE
-curl -X DELETE \
-  http://127.0.0.1:4000/api/v1/greet/gusdelact \
-  -H 'cache-control: no-cache' \
-  -H 'content-type: application/json' \
-  -H 'postman-token: bc41f11e-554c-d17a-deea-8a59ea5d0ba5' \
-  -d '{
-	"greeting":"gusgugus"
-}'
+  -d '{"saludo":"esto es desde un prueba unitaria"}'
